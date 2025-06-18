470d547fb03d47ca6f3bdf30f201a59fce628021
Lui-die-lui <lsg05h@naver.com>
2025-06-18 21:49:08 +0900

    Feat : first commit

---
+++ src/main/java/com/koreait/dbms_study/service/UserService.java
@@ -0,0 +1,43 @@
+package com.koreait.dbms_study.service;
+
+
+import com.koreait.dbms_study.dto.AddUserReqDto;
+import com.koreait.dbms_study.entity.User;
+import com.koreait.dbms_study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Map<String, String> addUser(AddUserReqDto addUserReqDto) {
+        Map<String, String> response = new HashMap<>();
+        int result = userRepository.addUser(addUserReqDto.toEntity(addUserReqDto));
+        if (result == 1) {
+            response.put("status", "success");
+            response.put("message","추가성공");
+            return response;
+        }
+        response.put("status","failed");
+        response.put("message","추가 실패");
+        return response;
+    }
+//
+//    public List<User> getUserList(){
+//        return userRepository.getUserList();
+//    }
+//
+//    public Map<String, Object> getUserByUserId() {
+//        User user = userRepository.getUserByUserId(userId);
+//        if (user.isEmpty()) {
+//            response.put("")
+//        }
+//    }
+}
